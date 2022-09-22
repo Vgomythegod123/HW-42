e91353ab2e86c6ebd8514214f5cd5f17e22688d2
Vgomythegod123 <114121256+Vgomythegod123@users.noreply.github.com>
2022-09-21 20:28:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function hover(){
+window.alert("This uses first-letter");
+}
+
+function test(){
+window.alert("This uses the onclick function");
+}
+
+function hover2(){
+window.alert("This uses the hover pseudoclass");
+}
